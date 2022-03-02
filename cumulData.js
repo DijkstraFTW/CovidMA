22ef716c03c5bc2c7b7683e4855ae330b5b307af
Youssef <youssef4581@gmail.com>
2022-03-02 22:57:27 +0100

    changed daily data src to owid

--- cumulData.js
+++ cumulData.js
@@ -19,17 +19,22 @@ function setCumulData(status, response) {
 
     data = response["MAR"];
 
+    n_cases = data["new_cases"];
+    n_deaths = data["new_deaths"];
     n_tests = data["new_tests"];
     cases = data["total_cases"];
     deaths = data["total_deaths"];
     tests = data["total_tests"];
     r0 = data["reproduction_rate"];
 
+
     document.getElementById("total-cases-data").innerHTML = cases.toLocaleString();
     document.getElementById("total-deaths-data").innerHTML = deaths.toLocaleString();
     document.getElementById("total-tests-data").innerHTML = tests.toLocaleString();
     document.getElementById("r0-data").innerHTML = r0;
     document.getElementById("new-tests-data").innerHTML = " +" + n_tests.toLocaleString() + " tests";
+    document.getElementById("new-cases-data").innerHTML = " +" + n_cases.toLocaleString() + " cases";
+    document.getElementById("new-deaths-data").innerHTML = " +" + n_deaths.toLocaleString() + " deaths";
 }
 
 function setNewData(status, response) {
@@ -38,15 +43,14 @@ function setNewData(status, response) {
 
     n_cases = data["cases"];
     n_deaths = data["deaths"];
-    n_recoveries = data["recoveries"];
+    //n_recoveries = data["recoveries"];
     n_dose1 = data["dose_1_last_day"];
     n_dose2 = data["dose_2_last_day"];
     n_dose3 = data["dose_3_last_day"];
     total_dose3 = data["dose_3_total"];
 
-    document.getElementById("new-cases-data").innerHTML = " +" + n_cases.toLocaleString() + " cases";
-    document.getElementById("new-deaths-data").innerHTML = " +" + n_deaths.toLocaleString() + " deaths";
-    document.getElementById("recoveries-data").innerHTML = " +" + n_recoveries.toLocaleString();
+
+    //document.getElementById("recoveries-data").innerHTML = " +" + n_recoveries.toLocaleString();
     document.getElementById("new-dose1-data").innerHTML = " +" + n_dose1;
     document.getElementById("new-dose2-data").innerHTML = " +" + n_dose2;
     document.getElementById("new-dose3-data").innerHTML = " +" + n_dose3;
