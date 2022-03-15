ba5086c54a32d481d4e19da0d8f7343435bfdd4d
Youssef <youssef4581@gmail.com>
2022-03-15 16:49:56 +0100

    vax chart changes

--- VaxPctChart.js
+++ VaxPctChart.js
@@ -1,5 +1,9 @@
 var vax1, vax2, remainder, obj
 
+var valueXPrecedent = 0,
+    valueYPrecedent = 0,
+    index = 0;
+
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
@@ -62,14 +66,15 @@ function setWaffleChart() {
         squareSize = 25,
         squareValue = 0,
         gap = 2,
-        theData = [];
+        theData = [],
+        index = 0;
 
     var color = ["#005bff", "#99bdff", "#E8070c"]
 
     d3.csv("data.csv", function(error, dataV) {
 
         //total
-        total = 30000000
+        total = d3.sum(dataV, function(d) { return d.population; });
 
         //value of a square
         squareValue = total / (widthSquares * heightSquares);
@@ -79,7 +84,7 @@ function setWaffleChart() {
             d.population = +d.population;
             d.units = (d.population / squareValue);
             theData = theData.concat(
-                Array(Math.floor(d.units + 1)).join(1).split('').map(function() {
+                Array(Math.floor(d.units + 2)).join(1).split('').map(function() {
                     return {
                         squareValue: squareValue,
                         units: d.units,
@@ -90,8 +95,12 @@ function setWaffleChart() {
             );
         });
 
+        theData.splice(theData.length - 1)
+
         width = (squareSize * widthSquares) + widthSquares * gap + 25;
         height = (squareSize * heightSquares) + heightSquares * gap + 25;
+
+        console.log(theData);
 
         var waffle = d3.select("#waffle")
             .append("svg")
@@ -103,19 +112,61 @@ function setWaffleChart() {
             .enter()
             .append("rect")
             .attr("width", squareSize)
-            .attr("height", squareSize)
+            .attr("height", function(d, i) {
+
+                let result = squareSize
+
+                // if (valueXPrecedent != 0) {
+
+                //     temp = valueXPrecedent
+                //     valueXPrecedent = 0
+                //     result = (squareSize - temp)
+
+                //     return result
+                // }
+
+                console.log(theData[i]["groupIndex"] + " " + (theData[i]["units"] - index));
+
+
+
+                if (((theData[i]["units"] - index) < 2) && (theData[i]["groupIndex"] == 2)) {
+                    temp2 = ((theData[i]["units"] - Math.floor(theData[i]["units"])))
+                    result = (temp2 * squareSize)
+                }
+
+
+                if ((theData[i]["units"] - index) < 1) {
+
+                    if (theData[i]["groupIndex"] == 1) {
+                        temp2 = vax1 / total
+                    } else {
+                        temp2 = (d.units - Math.floor(d.units))
+                    }
+
+                    valueXPrecedent = (temp2 * squareSize)
+
+                    result = (temp2 * squareSize)
+
+                    index = 0
+
+                    return result
+
+                }
+                index++;
+
+                return result
+            })
             .attr("fill", function(d) {
                 return color[d.groupIndex];
             })
             .attr("x", function(d, i) {
-
+                row = i % heightSquares;
+                return (heightSquares * squareSize) - ((row * squareSize) + (row * gap))
+            })
+            .attr("y", function(d, i) {
                 //group n squares for column
                 col = Math.floor(i / heightSquares);
                 return (col * squareSize) + (col * gap);
-            })
-            .attr("y", function(d, i) {
-                row = i % heightSquares;
-                return (heightSquares * squareSize) - ((row * squareSize) + (row * gap))
             })
             .append("title")
             .text(function(d, i) {
